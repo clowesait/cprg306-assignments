4095ad9e6308b1d99d5726466c26b0b1af80c401
clowesait <cody.lowe@edu.sait.ca>
2025-02-07 19:16:53 -0700

    Finished week 3 Assignment

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className="text-4xl text-center">CPRG 306: Web Development 2 - Assignments</h1>
       <h2 className="text-2xl text-center">By Cody Lowe - 000649534</h2>
       <a href = "/week-2">Week 2</a>
+      <a href = "/week-3">Week 3</a>
     </main>
   );
 }
