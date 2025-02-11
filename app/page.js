03e6ed6c92ec69b7b66f43bed9e55d4b057555a1
clowesait <cody.lowe@edu.sait.ca>
2025-02-11 13:59:53 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,8 +5,10 @@ export default function Home() {
     <main>
       <h1 className="text-4xl text-center">CPRG 306: Web Development 2 - Assignments</h1>
       <h2 className="text-2xl text-center">By Cody Lowe - 000649534</h2>
-      <a href = "/week-2">Week 2</a>
-      <a href = "/week-3">Week 3</a>
+      <ul>
+        <li><a href = "/week-2">Week 2</a></li>
+        <li><a href = "/week-3">Week 3</a></li>
+      </ul>
     </main>
   );
 }
